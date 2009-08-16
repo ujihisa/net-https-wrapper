abb760edc6fdfb32903044eaba1ad1948408b49c
ujihisa <ujihisa@gmail.com>
2009-08-15 18:32:17 -0700

    Fine.

---
+++ README.md
@@ -0,0 +1,15 @@
+# net/https wrapper
+
+    require 'net-https-wrapper'
+
+    a = Net::HTTP.post(
+      'https://www.google.com/accounts/ClientLogin',
+      {
+        'Email' => email,
+        'Passwd' => pass,
+        'service' => 'blogger',
+        'accountType' => 'HOSTED_OR_GOOGLE',
+        'source' => 'ujihisa-bloggervim-1'
+      }.map {|i, j| "#{i}=#{j}" }.join('&'),
+      {'Content-Type' => 'application/x-www-form-urlencoded'})
+    a.body.lines.to_a.maph {|i| i.split('=') }['Auth'].chomp
--- net-https-wrapper.rb
+++ net-https-wrapper.rb
@@ -19,16 +19,3 @@ class Net::HTTP
     i.__send__(method, uri.path, data, header)
   end
 end
-
-# sample code
-#    a = Net::HTTP.post(
-#      'https://www.google.com/accounts/ClientLogin',
-#      {
-#        'Email' => email,
-#        'Passwd' => pass,
-#        'service' => 'blogger',
-#        'accountType' => 'HOSTED_OR_GOOGLE',
-#        'source' => 'ujihisa-bloggervim-1'
-#      }.map {|i, j| "#{i}=#{j}" }.join('&'),
-#      {'Content-Type' => 'application/x-www-form-urlencoded'})
-#    a.body.lines.to_a.maph {|i| i.split('=') }['Auth'].chomp
